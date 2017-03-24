943a2cd53db3026e8b180439a8b490dcc6607c99
Ben Metzger <benjpac@gmail.com>
2017-03-24 09:43:40 -0700

    add remaining questions

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@
 $(document).ready(function()) {
   $("form#trackPicker").submit(function() {
     event.preventDefault();
-    var name = $("#input1").val();
-    var question1 = $("input:radio[name=operator]:checked").val();
-    var question2 = $("input:radio[name=operator]:checked").val();
-    var question3 = $("input:radio[name=operator]:checked").val();
-    var question4 = $("input:radio[name=operator]:checked").val();
-    var question5 = $("input:radio[name=operator]:checked").val();
+    var name = $("#name").val();
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+    var question5 = $("input:radio[name=question5]:checked").val();
     var rubyTrack;
     var androidTrack;
     var designTrack;
-    if (question1 === "val1") {
+    if (question1 === "large") {
       rubyTrack += ;
 
     } else if (question1 === "val2") {
