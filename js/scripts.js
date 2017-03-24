4b7d1b1f47abfa984ca0cb11d33478504944fa69
Ben Metzger <benjpac@gmail.com>
2017-03-24 09:59:21 -0700

    add functionality to calculate weights of first question and return a result in an alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
+var result;
+var calcResult = function(rubyTrack, androidTrack, designTrack) {
+  if (rubyTrack > androidTrack && designTrack) {
+    result = "ruby";
+  } else if (androidTrack > rubyTrack && designTrack) {
+    result = "android";
+  } else if (designTrack > rubyTrack && androidTrack) {
+    result = "design";
+  }
+  return result
+}
 
-$(document).ready(function()) {
+$(document).ready(function() {
   $("form#trackPicker").submit(function() {
     event.preventDefault();
+    debugger;
     var name = $("#name").val();
     var question1 = $("input:radio[name=question1]:checked").val();
     var question2 = $("input:radio[name=question2]:checked").val();
     var question3 = $("input:radio[name=question3]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
     var question5 = $("input:radio[name=question5]:checked").val();
-    var rubyTrack;
-    var androidTrack;
-    var designTrack;
+    var rubyTrack = 0;
+    var androidTrack = 0;
+    var designTrack = 0;
     if (question1 === "large") {
-      rubyTrack += ;
-
-    } else if (question1 === "val2") {
-      result = subtract(number1, number2);
-    } else if (question1 === "val3") {
-      result = multiply(number1, number2);
-    } else if (question1 === "val4") {
-      result = divide(number1, number2);
+      rubyTrack += .2;
+      androidTrack += 1;
+      designTrack += .5;
+    } else if (question1 === "small") {
+      rubyTrack += 1;
+      androidTrack += .2;
+      designTrack += .5;
     }
+    alert(calcResult(rubyTrack, androidTrack, designTrack));
   });
 });
